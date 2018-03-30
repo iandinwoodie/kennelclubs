2c9ab7a5eb5bbab449429af5b19727d4dc8f4d72
Ian Dinwoodie <dinwoodieian@gmail.com>
2018-03-29 20:44:04 -0400

    Updated akc output to a more human readable csv

---
+++ kclubs/clubs/akc.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import csv
+import os
+import requests
+
+
+def fetch_data(data_dir):
+    page = requests.get('http://www.akc.org/')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    selection = soup.find(id='breed-search')
+    with open(os.path.join(data_dir, 'akc.csv'), 'w') as outfile:
+        wr = csv.writer(outfile, lineterminator='\n')
+        for breed in selection.find_all('option'):
+            if breed['value'] != '':
+                wr.writerow([breed.text])
+
+
+if __name__ == '__main__':
+    club_dir = os.path.split(os.path.dirname(os.path.realpath(__file__)))[0]
+    data_dir = os.path.join(os.path.split(club_dir)[0], 'data')
+    fetch_data(data_dir)
